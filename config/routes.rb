1033e6c5c7858ec1dc45c990c1de3ee28403f6df
tonyc2700 <a.s.chapman.10@aberdeen.ac.uk>
2013-07-05 12:04:29 +0200

    Made /bikes the root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Biker::Application.routes.draw do
   resources :bikes
+
+  root 'bikes#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
