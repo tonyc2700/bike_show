f3dc4e03280a9f3a8e6aaadead56132d42d001b0
tonyc2700 <a.s.chapman.10@aberdeen.ac.uk>
2013-07-05 12:03:36 +0200

    Added update action

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -14,13 +14,26 @@ class BikesController < ApplicationController
   def create
     @bike = Bike.new(params[:bike].permit(:title, :body))
     @bike.save
+    flash.notice = "Bike '#{@bike.title}' Created"
     redirect_to bike_path(@bike)
   end
 
   def destroy
     @bike = Bike.find(params[:id])
     @bike.destroy
+    flash.notice = "Bike '#{@bike.title}' Deleted"
     redirect_to bikes_path
+  end
+
+  def edit
+    @bike = Bike.find(params[:id])
+  end
+
+  def update
+    @bike = Bike.find(params[:id])
+    @bike.update_attributes(params[:bike].permit(:title, :body))
+    flash.notice = "Bike '#{@bike.title}' Updated"
+    redirect_to bike_path(@bike)
   end
 
 end
