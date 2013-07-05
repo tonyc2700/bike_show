8078f3a97bae23315e878aee40b6ace3027f2be5
tonyc2700 <a.s.chapman.10@aberdeen.ac.uk>
2013-07-05 11:01:49 +0200

    Added create action

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -10,4 +10,13 @@ class BikesController < ApplicationController
   def new
     @bike = Bike.new
   end
+
+  def create
+    @bike = Bike.new
+    @bike.title = params[:title]
+    @bike.body = params[:body]
+    @bike.save
+    redirect_to bike_path(@bike)
+  end
+
 end
